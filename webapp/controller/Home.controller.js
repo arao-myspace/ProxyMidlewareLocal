87300a397e485a9b9183a44890ba9d9868cd5ba7
Adrian Rybka <adrian_rybka1989@wp.pl>
2021-04-30 23:11:57 +0200

    msg2 30-04-2021

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	"sap/ui/core/Fragment"
+], function (Controller, formatter, Fragment) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -10,6 +11,30 @@ sap.ui.define([
 
 		onInit: function () {
 
+		},
+
+		onItemPress: function (oEvent) {
+			var oSource = oEvent.getParameter("listItem");
+			var oView = this.getView();
+
+			// create popover
+			if (!this._pPopover) {
+				this._pPopover = Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.basicTemplate.view.Popover",
+					controller: this
+				}).then(function (oPopover) {
+					oView.addDependent(oPopover);
+					// oPopover.bindElement("/ProductCollection/0");
+					return oPopover;
+				});
+			}
+			this._pPopover.then(function (oPopover) {
+				oPopover.openBy(oSource);
+			});
+		},
+		onPopoverClose: function () {
+			this.byId("myPopover").close();
 		}
 	});
 });
