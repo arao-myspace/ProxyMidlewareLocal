4645829d32392f06df63caec8f784833b0247d22
Adrian Rybka <adrian_rybka1989@wp.pl>
2021-07-25 18:12:48 +0200

    Commit 25.07.2021

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -1,46 +1,74 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+    "sap/ui/core/Fragment",
+    "sap/ui/core/mvc/Controller",
+    "../model/formatter"
+], function (Fragment, Controller, formatter) {
     'use strict';
 
     return Controller.extend("sap.ui.demo.basicTemplate.controller.Details", {
 
-		formatter: formatter,
+        formatter: formatter,
 
-		onInit: function () {
+        onInit: function () {
             var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.getRoute("details").attachPatternMatched(this._onObjectMatched, this);
+            oRouter.getRoute("details").attachPatternMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function (oEvent) {
             var sPath = window.decodeURIComponent(oEvent.getParameter("arguments").Path);
             var oTable = this.getView().byId("tableDetails");
             //   var oBindingInfo = oTable.getBindingInfo("items");
             //   var oTemplate = 
-              oTable.bindAggregation("items", {
-                 path: "/" + sPath + "/SpfliToSbookNav",
-                 template: this.byId("colListItemTempl"),
-                 templateSherable: false
-                 });
+            oTable.bindAggregation("items", {
+                path: "/" + sPath + "/SpfliToSbookNav",
+                template: this.byId("colListItemTempl"),
+                templateSherable: false
+            });
             var oCalendar = this.byId("calendar1");
             oCalendar.bindAggregation("specialDates", {
                 path: "/" + sPath + "/SpfliToSbookNav",
                 template: this.byId("specialDate"),
                 templateSherable: false
             });
-		},
+        },
         onBackToHome: function (oEvent) {
             var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("home");
+            oRouter.navTo("home");
             this.getView().byId("tableDetails").unbindItems();
         },
         onDetailPress: function (oEvent) {
-			var oItem = oEvent.getSource();
-			var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("edit", {
-				Path: window.encodeURIComponent(oItem.getBindingContext().getPath().substr(1))
-			});
+            var oItem = oEvent.getSource();
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("edit", {
+                Path: window.encodeURIComponent(oItem.getBindingContext().getPath().substr(1))
+            });
 
+        },
+        handleCalendarSelect: function (oEvt) {
+            var oAnchor = oEvt.getSource();
+            var sPath = this.byId("calendar1").getBinding("specialDates").getPath();
+            if (!this._oPopover) {
+                Fragment.load( {
+                    id: "popoverCal",
+                    name: "sap.ui.demo.basicTemplate.view.CalendarPopover",
+                    controller: this
+                }).then(function(oPopover) {
+                    this._oPopover = oPopover;
+					this.getView().addDependent(this._oPopover);
+                    this._oPopover.bindElement(sPath);
+                    //Jeszcze trzeba podpiąć items do naszej listy
+                   // this.byId("listPop").bindItems(sPath);
+
+					this._oPopover.openBy(oAnchor);
+                }.bind(this));
+            } else {
+                if (this._oPopover.isOpen()) {
+                    this._oPopover.close();
+                }
+                this._oPopover.openBy(oAnchor);
+                this._oPopover.bindElement(sPath);
+                 //Jeszcze trzeba podpiąć items do naszej listy
+               // this.byId("listPop").bindItems(sPath);
+            }
         }
     })
-    });
+});
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,18 +2,39 @@ sap.ui.define([], function () {
 	"use strict";
 	return {
 		smokerStatus: function (sStatus) {
-			if (sStatus=='X') {
+			if (sStatus == 'X') {
 				return true;
 			} else {
 				return false;
 			}
 		},
 		getCalendarItemType: function (sClass) {
-			return "Type09";
+
+			switch (sClass) {
+				case 'C':
+					return "Type09";
+					break;
+				case 'F':
+					return "Type08";
+					break;
+				case 'Y':
+					return "Type07";
+					break;
+			}
 		},
 		getCalendarItemColor: function (sClass) {
-			return "#333333";
+			switch (sClass) {
+				case 'C':
+					return "#ccff33";
+					break;
+				case 'F':
+					return "#0066ff";
+					break;
+				case 'Y':
+					return "#ff6600";
+					break;
+			}
 		},
-		
+
 	};
 });
